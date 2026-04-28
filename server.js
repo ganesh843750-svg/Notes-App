e5a2acc9fd7c1338a8757e4f54abb38d0486b5ee
ganesh843750-svg <ganesh843750@gmail.com>
2026-04-28 22:18:33 +0530

    Modified port number

--- server.js
+++ server.js
@@ -55,5 +55,8 @@ app.get('/display/:id', async ( req, resp)=>{
 })
 
 
-
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+    
+})
